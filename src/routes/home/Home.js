cd8d95a8deffbf4ba16be4b664e67be717405889
user <nithint@qburst.com>
2016-09-16 17:44:59 +0530

    something done :|

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -7,27 +7,79 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 
 const title = 'React Starter Kit';
 
-function Home({data}) {
-  return (
-    <div className={s.container}>
-        <div className={s.leftContainer}>
-        
-        </div>
-        <div className={s.rightContainer}>
-        
-        </div>
-    </div>
-  );
-}
+class Home extends Component {
 
-Home.propTypes = {
-  data: PropTypes.string.isRequired
-};
+  constructor(props) {
+    super(props);
+    this.state = {file: '',imagePreviewUrl: ''};
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+
+
+
+  render() {
+
+ let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+    }
+
+
+
+    return (
+      <div className={s.container}>
+          <div className={s.leftContainer}>
+          
+
+
+<input className="fileInput" type="file" onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton" type="submit" onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+
+
+
+
+
+          </div>
+          <div className={s.rightContainer}>
+          
+          </div>
+      </div>
+    );
+  }  
+}
 
 export default withStyles(s)(Home);
